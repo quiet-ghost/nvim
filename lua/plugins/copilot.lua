0533e18a71dc081a309ae6aa9c93e1b826a1f103
K <ksclafani26@pm.me>
2025-03-28 19:26:26 -0400

    copilot completion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,8 @@
 return {
-  "zbirenbaum/copilot.lua",
+  "github/copilot.vim",
   event = "InsertEnter",
-  dependencies = { "zbirenbaum/copilot-cmp" },
   config = function()
-    require("copilot").setup({
-      suggestion = { enabled = true }, -- Disable built-in suggestions (use copilot-cmp)
-      panel = { enabled = false },      -- Disable panel
-      filetypes = {
-        ["*"] = true,                  -- Enable for all filetypes
-      },
-    })
-    require("copilot_cmp").setup()
-    require("copilot.suggestion").is_visible()
-    require("copilot.suggestion").accept(modifier)
-    require("copilot.suggestion").accept_word()
-    require("copilot.suggestion").accept_line()
-    require("copilot.suggestion").next()
-    require("copilot.suggestion").prev()
-    require("copilot.suggestion").dismiss()
-    require("copilot.suggestion").toggle_auto_trigger()
-    -- Integrate with nvim-cmp
+
+    vim.g.copilot_filetypes = { ["*"] = true }
   end,
 }
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -3,24 +3,21 @@ return {
   branch = "v3.x",
   dependencies = {
     { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "L3MON4D3/LuaSnip" },
   },
   config = function()
     local lsp_zero = require("lsp-zero")
     lsp_zero.on_attach(function(client, bufnr)
       lsp_zero.default_keymaps({ buffer = bufnr })
-    end)
+    end) -- Added end for on_attach function
 
-    -- LSP servers for all languages
-    require("lspconfig").pyright.setup({})          -- Python
-    require("lspconfig").tsserver.setup({})         -- JavaScript/TypeScript
-    require("lspconfig").clangd.setup({})           -- C++
-    require("lspconfig").html.setup({})             -- HTML
-    require("lspconfig").cssls.setup({})            -- CSS
-    require("lspconfig").lua_ls.setup(lsp_zero.nvim_lua_ls()) -- Lua
-    require("lspconfig").jdtls.setup({})            -- Java
-    require("lspconfig").tailwindcss.setup({})      -- Tailwind CSS
-  end,
+    -- LSP servers
+    require("lspconfig").pyright.setup({})
+    require("lspconfig").tsserver.setup({})
+    require("lspconfig").clangd.setup({})
+    require("lspconfig").html.setup({})
+    require("lspconfig").cssls.setup({})
+    require("lspconfig").lua_ls.setup(lsp_zero.nvim_lua_ls())
+    require("lspconfig").jdtls.setup({})
+    require("lspconfig").tailwindcss.setup({})
+  end -- End for config function
 }
