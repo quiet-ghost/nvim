060eefb81f75fd445516d3ecaa0d3590ecd3dedc
K <ksclafani26@pm.me>
2025-03-29 00:26:40 -0400

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "File Explorer" }) -- Open file explorer
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" }) -- Move selected lines down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" }) -- Move selected lines up
+
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" }) -- Join lines and keep cursor position
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" }) -- Scroll down and center
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" }) -- Scroll up and center
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result and center" }) -- Next search result and center
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result and center" }) -- Previous search result and center
+
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without overwriting register" }) -- Paste without overwriting register
+
+vim.keymap.set("n", "<leader>y", [["+y]], { desc = "Yank to system clipboard" }) -- Yank to system clipboard
+vim.keymap.set("v", "<leader>y", [["+y]], { desc = "Yank to system clipboard" }) -- Yank to system clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to system clipboard (line)" }) -- Yank to system clipboard (line)
+
+vim.keymap.set("n", "<leader>d", [["_d]], { desc = "Delete without overwriting register" }) -- Delete without overwriting register
+vim.keymap.set("v", "<leader>d", [["_d]], { desc = "Delete without overwriting register" }) -- Delete without overwriting register
+
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode" }) -- Exit insert mode
+
+vim.keymap.set("n", "Q", "<nop>", { desc = "Disable Q" }) -- Disable Q
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "tmux sessionizer" }) -- tmux sessionizer
+vim.keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format current buffer" }) -- Format current buffer
+
+vim.keymap.set("n", "<leader>s", "%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { desc = "Replace word under cursor" }) -- Replace word under cursor
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,9 +3,33 @@
 -- Add any additional options here
 vim.opt.number = true
 vim.opt.relativenumber = true
+
 vim.opt.scrolloff = 8          -- Keep 8 lines visible above/below cursor
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"    -- Always show the sign column
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
