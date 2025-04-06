7017a93dc5d773bd0d0279823693e160ed7f6511
K <ksclafani26@pm.me>
2025-04-05 21:28:14 -0400

    supermaven/keymaps/harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -45,3 +45,65 @@ vim.keymap.set("n", "<C-k>", "<C-w> k")
 vim.keymap.set("n", "<C-j>", "<C-w> j")
 vim.keymap.set("n", "<C-h>", "<C-w> h")
 vim.keymap.set("n", "<C-l>", "<C-w> l")
+
+--HARPOON settings
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+--harpoon keymaps
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+vim.keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+vim.keymap.set("n", "<C-h>", function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<C-j>", function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<C-k>", function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<C-l>", function()
+  harpoon:list():select(4)
+end)
+
+--harpoon Previous & Next buffers
+vim.keymap.set("n", "<M-S-A>", function()
+  harpoon:list():prev()
+end)
+vim.keymap.set("n", "<M-S-D>", function()
+  harpoon:list():next()
+end)
+
+--harpoon telescope local harpoon = require("harpoon")
+harpoon:setup({})
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
+end
+
+vim.keymap.set("n", "<C-e>", function()
+  toggle_telescope(harpoon:list())
+end, { desc = "Open harpoon window" })
