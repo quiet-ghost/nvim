1d9b2954bc4a98baf750818e3d14e254df5d8384
K <ksclafani26@pm.me>
2025-04-05 23:48:53 -0400

    a

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -43,16 +43,4 @@ return {
       })
     end,
   },
-  -- lspkind.lua
-local lspkind = require("lspkind")
-lspkind.init({
-  symbol_map = {
-    Supermaven = "",
-  },
-})
-
-vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", {fg ="#6CC644"})
-},
-
-
-
+}
