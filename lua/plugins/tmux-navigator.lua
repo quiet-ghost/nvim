243913c505ffea8e67d8316e7e7d52983d9f6d85
K <ksclafani26@pm.me>
2025-04-14 21:36:36 -0400

    tmux-nav still not fixed

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,10 +1,18 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
+  "christoomey/vim-tmux-navigator",
+  event = "VeryLazy",
+  config = function()
+    vim.g.tmux_navigator_no_mappings = 1
+    vim.g.tmux_navigator_verbose = 1  -- Enable verbose output for debugging
+    vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { desc = "Go to the left pane", silent = true })
+    vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { desc = "Go to the down pane", silent = true })
+    vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { desc = "Go to the up pane", silent = true })
+    vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { desc = "Go to the right pane", silent = true })
+    vim.keymap.set("n", "<C-\\>", ":TmuxNavigatePrevious<CR>", { desc = "Go to the previous pane", silent = true })
+    vim.keymap.set("t", "<C-h>", "<C-\\><C-n>:TmuxNavigateLeft<CR>", { desc = "Go to the left pane", silent = true })
+    vim.keymap.set("t", "<C-j>", "<C-\\><C-n>:TmuxNavigateDown<CR>", { desc = "Go to the down pane", silent = true })
+    vim.keymap.set("t", "<C-k>", "<C-\\><C-n>:TmuxNavigateUp<CR>", { desc = "Go to the up pane", silent = true })
+    vim.keymap.set("t", "<C-l>", "<C-\\><C-n>:TmuxNavigateRight<CR>", { desc = "Go to the right pane", silent = true })
+    vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>:TmuxNavigatePrevious<CR>", { desc = "Go to the previous pane", silent = true })
+  end,
 }
