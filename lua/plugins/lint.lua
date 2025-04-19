73ecb1a34c89b7da8d16a4cd6dde8b0f0f2b2a4b
K <ksclafani26@pm.me>
2025-04-19 00:04:19 -0400

    snacks

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local lint = require("lint")
     lint.linters_by_ft = {
-      python = { "flake8" },
+      python = { "ruff" },
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       ["javascriptreact"] = { "eslint_d" },
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,13 +1,16 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    explorer = {
+      replace_netrw = true,
+    },
     dashboard = {
       sections = {
-      { section = "header" },
-      { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-      { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-      { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-      { section = "startup" },
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
       },
     },
   },
