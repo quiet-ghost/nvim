590b73abf15e387d627436eb09f849446d5360f7
K <ksclafani26@pm.me>
2025-05-11 15:49:24 -0400

    cmp update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,6 +6,7 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
@@ -24,16 +25,45 @@ return {
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<M-n>"] = cmp.mapping.select_next_item(),
+          ["<M-p>"] = cmp.mapping.select_prev_item(),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
           ["<C-e>"] = cmp.mapping.abort(),
         }),
         sources = {
           { name = "nvim_lsp" },
           { name = "luasnip" },
+          { name = "cmdline" },
           { name = "buffer" },
           { name = "path" },
+        },
+      })
+
+      -- Set up command-line completion
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+          { name = "cmdline" },
+          { name = "luasnip" },
+        }),
+      })
+
+      -- Set up search completion with highlighting
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+        view = {
+          entries = { name = "wildmenu", separator = "|" },
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.kind = "Text"
+            vim_item.menu = "Search"
+            return vim_item
+          end,
         },
       })
 
