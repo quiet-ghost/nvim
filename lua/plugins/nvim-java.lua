f321738b04d93608743ece6ae42bf708687f3242
K <ksclafani26@pm.me>
2025-08-02 10:33:24 -0400

    wip

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -34,8 +34,8 @@ return {
       "williamboman/mason.nvim",
       opts = {
         registries = {
-          "github:nvim-java/mason-registry",
           "github:mason-org/mason-registry",
+          "github:nvim-java/mason-registry",
         },
       },
     },
