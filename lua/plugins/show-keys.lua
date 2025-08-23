28d994b8e49625ddf0ca335ad0c0b191f25b9206
K <ksclafani26@pm.me>
2025-08-22 23:27:50 -0400

    showkeys?

---
+++ lua/plugins/show-keys.lua
@@ -0,0 +1,12 @@
+return {
+  cmd = "ShowKeysToggle",
+  "nvzone/showkeys",
+  last = false,
+  opts = {
+    timeout = 1,
+    maxkeys = 7,
+    show_count = true,
+    excluded_modes = { "i" },
+    position = "top-right",
+  },
+}
