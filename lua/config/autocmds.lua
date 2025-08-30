fac3da85f2532117dca13b4d9ff42fc634208072
K <ksclafani26@pm.me>
2025-08-30 12:15:34 -0400

    feat: autogenerate boilerplate for java

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,16 @@
 --   end,
 --   desc = "Auto-start showkeys on Neovim startup",
 -- })
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*.java",
+  callback = function()
+    local filename = vim.fn.expand("%:t:r")
+    local lines = {
+      "public class " .. filename .. " {",
+      "    ",
+      "}",
+    }
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    vim.api.nvim_win_set_cursor(0, { 2, 4 })
+  end,
+})
